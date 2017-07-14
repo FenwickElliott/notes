deefe3d6408a666bb5c52cbf8351b5e76e942415
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-07-14 10:35:59 -0400

    bubbeling corrected upto but not including root

--- Heap/heap.rb
+++ Heap/heap.rb
@@ -8,52 +8,42 @@ class Heap
         @tail.left ? @tail.right = node : @tail.left = node
         node.up = @tail
 
-        swap(node)
+        while node.rating > node.up.rating
+            bubble(node)
+            return puts 'rooted' if node.up == @root
+        end
         move_tail
     end
 
-    def swap(o)
-        # puts o.name
+    def bubble(o)
         l = o.up
-        while l && o.rating > l.rating
+        l_up = l.up
+        l_left = l.left
+        l_right = l.right
 
-            # puts o.name
-        
-            l_up = l.up
-            l_left = l.left
-            l_right = l.right
+        o_up = o.up
+        o_left = o.left
+        o_right = o.right
 
-            o_up = o.up
-            o_left = o.left
-            o_right = o.right
+        l_up.left == l ? l_up.left = o : l_up.right = o
+        o.up = l_up
 
-            l_up.left == l ? l_up.left = o : l_up.right = o
-            o.up = l_up
+        l.up = o
 
-            l.up = o
+        o.left.up = l if o.left
+        o.right.up = l if o.right
 
+        l.right = o_right
+        l.left = o_left
 
-            o.left.up = l if o.left
-            o.right.up = l if o.right
-
-            l.right = o_right
-            l.left = o_left
-
-            # puts l.name
-            # puts o.name
-
-            if l_left == o
-                o.left = l
-                o.right = l_right
-            elsif l_right == o
-                o.right = l
-                o.left = l_left
-            end
-
-            # o = o.up
-            l = o.up
-
+        if l_left == o
+            o.left = l
+            o.right = l_right
+        elsif l_right == o
+            o.right = l
+            o.left = l_left
         end
+        o = o.up
     end
 
     def move_tail
