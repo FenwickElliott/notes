be8240475882db754d542f25f0caca202616f5cd
Charles Fenwick Elliott <Charles@FenwickElliott.io>
2017-07-18 09:25:59 -0400

    Complete and refactored

--- Heap/heap.rb
+++ Heap/heap.rb
@@ -7,48 +7,33 @@ class Heap
         return @tail = @root = node unless @root
         @tail.left ? @tail.right = node : @tail.left = node
         node.up = @tail
-
-        
         bubble(node)
         move_tail
-        printf
-        puts
     end
 
     def bubble(o)
         if o.rating > o.up.rating
 
             l = o.up
-            # puts "Bubbeling #{o.name} over #{l.rating}"
             l_up = l.up ; l_left = l.left ; l_right = l.right
             o_up = o.up ; o_left = o.left ; o_right = o.right
-
-            # l_up.left == l ? l_up.left = o : l_up.right = o
 
             # liaus up
             l.up = o
 
             # oedipus down
-            l_left == o ? (o.left = l ; o.right = l_right ) : (o.left = l_left ; o.right = l)
+            l_left == o ? (o.left = l ; o.right = l_right ; o.right.up = o if o.right) : (o.left = l_left ; o.right = l)
 
             if l_left == o
-            # if true
-                puts "Left: Bubbeling #{o.name} over #{l.name}"
                 o.left = l
                 o.right = l_right
                 o.right.up = o if o.right
             elsif l_right == o
-                puts "Right: Bubbeling #{o.name} over #{l.name}"
-                o.printn
-                puts
-                l.printn
-                puts
                 o.left = l_left
                 o.right = l
                 o.left.up = o if o.left
             end
 
-            
             # liaus down
             l.left = o_left ; l.right = o_right
 
@@ -56,15 +41,12 @@ class Heap
             o_left.up = l if o_left
             o_right.up = l if o_right
 
-
             # grandparent down & oedipus up unless reroot
             if l == @root
-                puts 'rerooting'
                 @root = o
             else
                 o.up = l_up
                 l_up.left == l ? l_up.left = o : l_up.right = o
-
                 bubble(o)
             end
         end
@@ -74,30 +56,30 @@ class Heap
     def move_tail
         ar = [@root]
         ar.each do |x|
-            x.left ? ar << x.left : ( puts "Tail " + @tail.name ; return @tail = x)
-            x.right ? ar << x.right : ( puts "Tail " + @tail.name ; return @tail = x)
+            x.left ? ar << x.left : ( return @tail = x )
+            x.right ? ar << x.right : ( return @tail = x )
         end
     end
 
-      # Recursive Depth First Search
-  def find(root = @root, data)
-    finder = @root
-    while finder.name != data
-      self.find(nil) if data && data == @root.looking_for
-      if finder.left && finder.left.looking_for != data
-        finder = finder.left
-        finder.looking_for = data
-      elsif finder.right && finder.right.looking_for != data
-        finder = finder.right
-        finder.looking_for = data
-      elsif finder.up
-        finder = finder.up
-      else
-        return nil
-      end
+    def find(data)
+        finder = @root
+        while finder.name != data
+            self.find(nil) if data && data == @root.looking_for
+            if finder.left && finder.left.looking_for != data
+                finder = finder.left
+                finder.looking_for = data
+            elsif finder.right && finder.right.looking_for != data
+                finder = finder.right
+                finder.looking_for = data
+            elsif finder.up
+                finder = finder.up
+            else
+                return nil
+            end
+        end
+        puts finder.printn
+        puts
     end
-    finder.printn
-  end
     
     def printf
         ar = [@root]
